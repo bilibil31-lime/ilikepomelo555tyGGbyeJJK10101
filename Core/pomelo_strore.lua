423fc8c8d01169562e70d95116100e30a3e8e60d
bilibil31-lime <bilibil31.lemonlike@gmail.com>
2026-07-22 20:41:21 +0700

    Update pomelo_strore.lua

--- Core/pomelo_strore.lua
+++ Core/pomelo_strore.lua
@@ -4,9 +4,25 @@ script1 name=fkfkfkf hub
 Description=ifjihgjrijgifjigjif
 price=191
 time=20d
+uidmap=
 -----------------
 script2 name=fkfkfkf hubdfjijd
 =loadstring(game:HttpGet("https://pastebin.com/j9qasdFp0"))()
 Description=ifjihgjrijsdfjigjif
 price=1
 time=1m
+uidmap=
+-----------------
+script1 name=fkfkfkf hub
+=loadstring(game:HttpGet("https://pastebin.com/j9qaEFp0"))()
+Description=ifjihgjrijgifjigjif
+price=191
+time=2000000000000000000m
+uidmap=
+-----------------
+script2 name=fkfkfkf hubdfjijd
+=loadstring(game:HttpGet("https://pastebin.com/j9qasdFp0"))()
+Description=ifjihgjrijsdfjigjif
+price=1
+time=48598498594859485
+uidmap=
